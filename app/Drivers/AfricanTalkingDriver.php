b61eb246c8967280e5d59933a0bda421223cd592
Helen Ndip <helenndipasobi@gmail.com>
2022-06-15 08:02:37 +0100

    botman controller get request

--- app/Drivers/AfricanTalkingDriver.php
+++ app/Drivers/AfricanTalkingDriver.php
@@ -9,7 +9,8 @@ use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
 use BotMan\BotMan\Messages\Outgoing\Question;
 use BotMan\BotMan\Users\User;
 use Illuminate\Support\Collection;
-use Symfony\Component\HttpFoundation\Request;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AfricanTalkingDriver
 {
@@ -21,143 +22,25 @@ class AfricanTalkingDriver
      * Build payload from incoming request.
      * @param Request $request
      */
-    public function buildPayload(Request $request)
+    public function buildReply($payload)
     {
-        $data = $request->request->all();
+        $text = "Sorry, your phone number is not registered for this service. Good Bye.";
 
-        $payload = [
-            'driver' => 'web',
-            'isActive' => isset($data['isActive']) ?? 0,
-            'sessionId' => $data['sessionId'] ?? null,
-            'direction' => $data['direction'] ?? null,
-            'callerNumber' => $data['callerNumber'] ?? null,
-            'destinationNumber' => $data['destinationNumber'] ?? null,
-            'dtmfDigits' => $data['dtmfDigits'] ?? null,
-            'recordingUrl' => $data['recordingUrl'] ?? null,
-            'durationInSeconds' => $data['durationInSeconds'] ?? null,
-            'currencyCode' => $data['currencyCode'] ?? null,
-            'amount' => $data['amount'] ?? null,
-        ];
-
-        $this->payload = $payload;
-        $this->event = Collection::make(array_merge($data, $payload));
-        $this->config = Collection::make($this->config->get('web', []));
-    }
-
-    /**
-     * Take all the outgoing messages and build a reply understandable by
-     * Africa's Talking USSD gateway.
-     * @param array $messages
-     * @return string|null
-     */
-    protected function buildReply($messages)
-    {
-        if (empty($messages)) {
-            return;
-        }
-
-        $sessionIsCompleted = false;
-        $replies = [];
-
-        foreach ($messages as $message) {
-            if ($message instanceof OutgoingMessage || $message instanceof Question) {
-                $replies[] = $message->getText();
-            }
-
-            if ($message instanceof LastScreen && ! $message->getCurrentPage()->hasNext()) {
-                $sessionIsCompleted = true;
-            }
-        }
-
-        $reply = $sessionIsCompleted ? 'END ' : 'CON ';
-
-        $reply .= implode("\n", $replies);
-
-        return $reply;
-    }
-    /**
-     * Returns true if incoming request matches the expected by this driver.
-     *
-     * @return bool
-     */
-    public function matchesRequest(): bool
-    {
-        $africasTalkingKeys = ['isActive', 'sessionId', 'direction', 'callerNumber', 'destinationNumber'];
-
-        foreach ($africasTalkingKeys as $key) {
-            if (is_null($this->event->get($key))) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * Retrieve the chat message.
-     *
-     * @return array
-     */
-    public function getMessages()
-    {
-        if (empty($this->messages)) {
-            $message = $this->event->get('recordingUrl');
-            $userId = $this->event->get('callerNumber');
-            $this->messages = [new IncomingMessage($message, $userId, $userId, $this->payload)];
-        }
-        return $this->messages;
-    }
-    /**
-     * Retrieve User information.
-     * @param IncomingMessage $matchingMessage
-     * @return User
-     */
-    public function getUser(IncomingMessage $matchingMessage)
-    {
-        return new User();
-    }
-
-    /**
-     * @param IncomingMessage $message
-     * @return \BotMan\BotMan\Messages\Incoming\Answer
-     */
-    public function getConversationAnswer(IncomingMessage $message)
-    {
-        $response = $this->buildReply($this->$message);
-        return Answer::create($message->getText())->setMessage($response);
-    }
-
-    /**
-     * @param string|Question|OutgoingMessage $message
-     * @param IncomingMessage $matchingMessage
-     * @param array $additionalParameters
-     * @return array
-     */
-    public function buildServicePayload($message, $matchingMessage, $additionalParameters = [])
-    {
-        if (! $message instanceof Question && ! $message instanceof OutgoingMessage) {
-            $this->errorMessage = 'Unsupported message type.';
-            $this->replyStatusCode = 500;
-        }
-        return [
-            'message' => $message,
-            'additionalParameters' => $additionalParameters,
-        ];
-    }
-
-    /**
-     * @param mixed $payload
-     * @return Response
-     */
-    public function sendPayload($payload)
-    {
-        $response = $this->http->post($this->endpoint . 'send', [], $payload, [
-//            "Authorization: Bearer {$this->config->get('token')}",
-            'Content-Type: application/json',
-            "apiKey: {$this->apikey}",
-            'Accept: application/json',
-        ], true);
+        // Compose the response
+        $response  = '<?xml version="1.0" encoding="UTF-8"?>';
+        $response .= '<Response>';
+        $response .= '<Say>'.$text.'</Say>';
+        $response .= '</Response>';
 
         return $response;
     }
+
+    public function buildResponseheaders(Response $response) {
+        $response->withHeaders([
+            'Content-type' => 'text/plain',
+            'apiKey' => this->$apikey
+        ]);
+    }
+
+
 }
--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -2,11 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Drivers\AfricanTalkingDriver;
 use Illuminate\Http\Request;
 
 class BotManController extends Controller
 {
-    public function handle() {
-        return "hello Africa Talking";
+    public function handle(Request $request) {
+
+        $xmlData = $request->getContent();
+        $data = $request->request->all();
+
+        $xml = simplexml_load_string($xmlData);
+
+        error_log($xml->isActive . ' read isActive from xml');
+        error_log($xml->sessionId . ' read sessionid from xmll');
+
+        error_log(json_encode($data));
+
+        $payload = [
+            'isActive' => isset($data['isActive']) ?? 0,
+            'sessionId' => $data['sessionId'] ?? null,
+            'direction' => $data['direction'] ?? null,
+            'callerNumber' => $data['callerNumber'] ?? null,
+            'destinationNumber' => $data['destinationNumber'] ?? null,
+            'dtmfDigits' => $data['dtmfDigits'] ?? null,
+            'recordingUrl' => $data['recordingUrl'] ?? null,
+            'durationInSeconds' => $data['durationInSeconds'] ?? null,
+            'currencyCode' => $data['currencyCode'] ?? null,
+            'amount' => $data['amount'] ?? null,
+        ];
+
+        error_log( json_encode($payload) . ' ** read post form data from request' );
+
+
+        if ($payload->get('isActive') == 1)  {
+            $handler = new AfricanTalkingDriver();
+            return "Hello Africa Talking";
+//            return $handler->buildReply($payload);
+        }
+        return "Unrecognized request";
     }
 }
